c126a4378d9c40127d712f4ab130a69a1b444651
Ali Abdollahi <63149242+AliAbdollahiii@users.noreply.github.com>
2023-03-22 01:58:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,73 @@
 from flask import Flask, jsonify, request
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
+import os
 
+# Create the Flask app
 app = Flask(__name__)
-app.config['JWT_SECRET_KEY'] = 'super-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
+# Set up the Flask-JWT-Extended extension
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY') or 'super-secret'  # Change this!
 jwt = JWTManager(app)
+
+# Set up the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# Create a user model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(100))
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+# Create the database tables
+with app.app_context():
+    db.create_all()
 
+# Register a new user
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if not username or not password:
+        return jsonify({'msg': 'Username and password are required.'}), 400
+
+    user = User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({'msg': 'User registered successfully.'}), 201
+
+# Log in an existing user
 @app.route('/login', methods=['POST'])
 def login():
     username = request.json.get('username', None)
     password = request.json.get('password', None)
+    user = User.query.filter_by(username=username, password=password).first()
+    if not user:
+        return jsonify({'msg': 'Invalid username or password.'}), 401
 
-    user = User.query.filter_by(username=username).first()
-
-    if not user or not user.password == password:
-        return jsonify({'message': 'Invalid credentials'}), 401
-
-    access_token = create_access_token(identity=username)
+    access_token = create_access_token(identity=user.id)
     return jsonify({'access_token': access_token}), 200
 
-@app.route('/protected', methods=['GET'])
-@jwt_required
-def protected():
-    current_user = get_jwt_identity()
-    return jsonify({'message': f'Hello, {current_user}!'}), 200
+# Get the current user
+@app.route('/me', methods=['GET'])
+@jwt_required()
+def me():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    return jsonify({'username': user.username}), 200
 
+# Access a protected route
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    return jsonify({'msg': 'You are accessing a protected route.'}), 200
+
+# Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)  # Change the port if necessary
